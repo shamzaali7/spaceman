21238ea52f9f84f9c432fc691d57193458989c30
Hamzaali7 <Hamzaali7@yahoo.com>
2022-10-24 15:37:04 -0400

    mvp done

--- script.js
+++ script.js
@@ -6,11 +6,12 @@ const select = document.querySelector(".button-word");
 const selectLetter = document.querySelector(".button-letter");
 let input = document.querySelector(".user-word");
 let inputLetter = document.querySelector(".user-letter");
-let inputArray = [];
+let inputChecker = "";
+let counterCorrect = 0;
+let counterIncorrect = 1;
 
 select.addEventListener("click", e => {
     e.preventDefault();
-    console.log(input.value.length);
     for (let i=0; i<input.value.length; i++){
         let letters = document.createElement("div");
         lettersContainer.appendChild(letters);
@@ -25,20 +26,43 @@ select.addEventListener("click", e => {
 
 selectLetter.addEventListener("click", e => {
     e.preventDefault();
-    console.log(inputLetter.value)
     for(let i=0; i<input.value.length; i++){
         if(inputLetter.value == input.value.charAt(i).toLowerCase()){
             let letterHolder = document.querySelector(".div-"+i);
             letterHolder.classList.remove("invisible");
-            
+            inputChecker = inputLetter.value;
+            counterCorrect++;
+            checkWin();
         }
     }
+        if(inputChecker == ""){
+            alert("Incorrect letter")
+            let off = document.querySelector(`.ufo-${counterIncorrect}`);
+            off.classList.add("offline");
+            let on = document.querySelector(`.ufo-${(counterIncorrect+1)}`);
+            on.classList.remove("offline");
+            if(counterIncorrect+1 === 8){
+                alert(`Sorry, you've made too many incorrect guesses, the word is ${input.value}`);
+                charInputContainer.classList.add("offline");
+                for(let i=0; i<input.value.length; i++){
+                    let letterIdentifier = document.querySelector(".div-"+i);
+                    letterIdentifier.classList.remove("invisible");
+                }
+            }
+            counterIncorrect++;
+        }
     inputLetter.value = "";
+    inputChecker = "";
 })
 
 
-// function makeArray(){
-//     for (let i=0; i<input.value.length; i++){
-//         inputArray[i] = input.value.charAt(i)
-//     }
-// }
+function checkWin(){
+    if(counterCorrect == input.value.length){
+        alert("You Win!");
+        charInputContainer.classList.add("offline");
+        document.querySelector(`.ufo-${counterIncorrect}`).classList.add("offline");
+        document.querySelector(".showWin").classList.remove("offline");
+    }
+}
+
+//https://i.imgur.com/XPi9r81.png
