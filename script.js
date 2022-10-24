d34b471ebb29b93bd831092aa1413f47042f91ec
Hamzaali7 <Hamzaali7@yahoo.com>
2022-10-23 22:40:23 -0400

    Day 2/weekend

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 //Psuedo code: grab 
 const lettersContainer = document.querySelector(".containerLetters");
+const inputContainer = document.querySelector(".containerInput");
+const charInputContainer = document.querySelector(".containerInputChar");
 const select = document.querySelector(".button-word");
 let input = document.querySelector(".user-word");
+let input2 = document.querySelector(".user-word-2");
+let inputArray = [];
 
 select.addEventListener("click", e => {
     console.log(input.value.length);
     for (let i=0; i<input.value.length; i++){
         let letters = document.createElement("div");
         lettersContainer.appendChild(letters);
-        letters.classList.add("underLine");
-        letters.classList.add("invisible")
+        letters.classList.add("invisible");
+        letters.classList.add(`${i}`);
+        letters.style.flex = 1;
+        letters.innerHTML = input.value.charAt(i)
     }
+    inputContainer.classList.add("offline")
+    charInputContainer.classList.remove("offline")
+
 })
+function makeArray(){
+    for (let i=0; i<input.value.length; i++){
+        inputArray[i] = input.value.charAt(i)
+    }
+}
