2994226c4482f85460781fdfe9d504722de2815f
Hamzaali7 <Hamzaali7@yahoo.com>
2022-10-24 12:01:39 -0400

    main functionality working, need to add ufo

--- script.js
+++ script.js
@@ -1,28 +1,44 @@
 //Psuedo code: grab 
 const lettersContainer = document.querySelector(".containerLetters");
 const inputContainer = document.querySelector(".containerInput");
-const charInputContainer = document.querySelector(".containerInputChar");
+const charInputContainer = document.querySelector(".containerInputLetter");
 const select = document.querySelector(".button-word");
+const selectLetter = document.querySelector(".button-letter");
 let input = document.querySelector(".user-word");
-let input2 = document.querySelector(".user-word-2");
+let inputLetter = document.querySelector(".user-letter");
 let inputArray = [];
 
 select.addEventListener("click", e => {
+    e.preventDefault();
     console.log(input.value.length);
     for (let i=0; i<input.value.length; i++){
         let letters = document.createElement("div");
         lettersContainer.appendChild(letters);
+        letters.classList.add("div-"+i);
         letters.classList.add("invisible");
-        letters.classList.add(`${i}`);
         letters.style.flex = 1;
-        letters.innerHTML = input.value.charAt(i)
+        letters.innerHTML = input.value.charAt(i).toLowerCase()
     }
     inputContainer.classList.add("offline")
     charInputContainer.classList.remove("offline")
-
 })
-function makeArray(){
-    for (let i=0; i<input.value.length; i++){
-        inputArray[i] = input.value.charAt(i)
+
+selectLetter.addEventListener("click", e => {
+    e.preventDefault();
+    console.log(inputLetter.value)
+    for(let i=0; i<input.value.length; i++){
+        if(inputLetter.value == input.value.charAt(i).toLowerCase()){
+            let letterHolder = document.querySelector(".div-"+i);
+            letterHolder.classList.remove("invisible");
+            
+        }
     }
-}
+    inputLetter.value = "";
+})
+
+
+// function makeArray(){
+//     for (let i=0; i<input.value.length; i++){
+//         inputArray[i] = input.value.charAt(i)
+//     }
+// }
