dbfa7a1a9771a9ce8be7de8c4d7f8e184fa1071e
Hamzaali7 <Hamzaali7@yahoo.com>
2022-10-25 16:44:09 -0400

    cleaned up code

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const inputContainer = document.querySelector(".containerInput");
 const charInputContainer = document.querySelector(".containerInputLetter");
 const selectWord = document.querySelector(".button-word");
 const selectLetter = document.querySelector(".button-letter");
-const usedLetters = document.querySelector(".box-1")
+const displayUsedLetters = document.querySelector(".display-used-letters")
 let input = document.querySelector(".user-word");
 let inputLetter = document.querySelector(".user-letter");
 let inputChecker = "";
@@ -31,21 +31,14 @@ selectWord.addEventListener("click", e => {
 
 selectLetter.addEventListener("click", e => {
     e.preventDefault();
-    console.log(checkerUsed, "checkerUsed")
-    console.log(alreadyUsed)
     if(alreadyUsed.length){
         for(let i=alreadyUsed.length; i>=0; i--){
-            console.log(alreadyUsed[i], "alreadyUsed")
-            console.log(inputLetter.value, "inputLetter")
             if (alreadyUsed[i] === inputLetter.value.toUpperCase()){ //or == alreadyUsed[alreadyUsed.length-1].value
-                console.log(alreadyUsed[i], "alreadyUsed41")
-                console.log(inputLetter.value, "inputLetter42")
                 checkerUsed++
             }
         }
     }
     alreadyUsed[counterLetter] = inputLetter.value.toUpperCase();
-    console.log(checkerUsed, "checkerUsed")
     if(checkerUsed > 0){
         alert("you have already used that character");
         alreadyUsed.splice(counterLetter);
@@ -61,25 +54,25 @@ selectLetter.addEventListener("click", e => {
                 checkWin();
             }
         }
-            if(inputChecker == ""){
-                alert("Incorrect letter")
-                let off = document.querySelector(`.ufo-${counterIncorrect}`);
-                off.classList.add("offline");
-                let on = document.querySelector(`.ufo-${(counterIncorrect+1)}`);
-                on.classList.remove("offline");
-                if(counterIncorrect+1 === 8){
-                    alert(`Sorry, you've made too many incorrect guesses, the word is ${input.value}`);
-                    charInputContainer.classList.add("offline");
-                    for(let i=0; i<input.value.length; i++){
-                        let letterIdentifier = document.querySelector(".div-"+i);
-                        letterIdentifier.classList.remove("invisible");
-                    }
+        if(inputChecker == ""){
+            alert("Incorrect letter")
+            let off = document.querySelector(`.ufo-${counterIncorrect}`);
+            off.classList.add("offline");
+            let on = document.querySelector(`.ufo-${(counterIncorrect+1)}`);
+            on.classList.remove("offline");
+            if(counterIncorrect+1 === 8){
+                alert(`Sorry, you've made too many incorrect guesses, the word is ${input.value}`);
+                charInputContainer.classList.add("offline");
+                for(let i=0; i<input.value.length; i++){
+                    let letterIdentifier = document.querySelector(".div-"+i);
+                    letterIdentifier.classList.remove("invisible");
                 }
-                counterIncorrect++;
             }
+            counterIncorrect++;
+        }
         
     }
-    usedLetters.innerHTML = alreadyUsed;
+    displayUsedLetters.innerHTML = alreadyUsed;
     inputLetter.value = "";
     inputChecker = "";
     checkerUsed = 0;
